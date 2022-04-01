9b28a3543d23659900e39f60f2fbe760db694efb
Minh <minh.tranimddtcc@hcmut.edu.vn>
2022-04-01 17:43:12 +0700

    Complete Drawer Sidebar

---
+++ apps/components/CustomDrawer.js
@@ -0,0 +1,24 @@
+import { View, Text, Image, ImageBackground } from 'react-native'
+import React from 'react'
+import {DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer'
+
+
+const CustomDrawer = (props) => {
+  return (
+    <View style = {{flex:1}}>
+        <DrawerContentScrollView {...props}>
+            <ImageBackground source={require('../assets/images/Background.jpg')} style={{padding: 20, marginTop: -4}}>
+                <Image source={require('../assets/images/Avatar.jpg')} 
+                    style={{height: 80, width: 80, borderRadius: 40, marginBottom: 10}}
+                />
+                <Text style={{color: '#fff', fontFamily: 'Nunito-Bold', fontSize: 18}}>Sơn Nguyễn</Text>
+            </ImageBackground> 
+            <View style={{flex:1, backgroundColor: '#fff', paddingTop: 10}}>
+                <DrawerItemList {...props} />
+            </View>
+        </DrawerContentScrollView>
+    </View>
+  )
+}
+
+export default CustomDrawer
--- apps/routes/MenuDrawer.js
+++ apps/routes/MenuDrawer.js
@@ -4,24 +4,42 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Dashboard from '../screens/Dashboard';
 import Activity from '../screens/Activity';
 import Account from '../screens/Account';
-
+import CustomDrawer from '../components/CustomDrawer';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const drawer = createDrawerNavigator();
 
 export default function Drawer() {
     return (
-        <drawer.Navigator>
+        <drawer.Navigator 
+            drawerContent={props => <CustomDrawer {...props}/>}
+            screenOptions={{
+                drawerLabelStyle: {marginLeft: -25, fontFamily: 'Nunito-Medium', fontSize: 18},
+                drawerActiveTintColor: '#fff',
+                drawerActiveBackgroundColor: '#016BD9',
+                drawerInactiveTintColor: '#333'
+            }}            
+        >
             <drawer.Screen
                 name='Dashboard'
                 component={Dashboard}
+                options={{
+                    drawerIcon: ({color}) => (<MaterialIcons name = 'home' size={25} color= {color}/>)
+                }}
                 />
             <drawer.Screen
                 name='Activity'
                 component={Activity}
+                options={{
+                    drawerIcon: ({color}) => (<MaterialIcons name = 'history' size={25} color= {color}/>)
+                }}
                 />
             <drawer.Screen
                 name='Account'
                 component={Account}
+                options={{
+                    drawerIcon: ({color}) => (<MaterialIcons name = 'person' size={25} color= {color}/>)
+                }}
                 />
         </drawer.Navigator>
     )
