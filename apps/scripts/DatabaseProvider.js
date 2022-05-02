21e5c665c656bd18b4c99eae1ca6d27576d86a18
DecSP <nguyencaoson7921@gmail.com>
2022-05-02 15:49:04 +0700

    ayaya

--- apps/scripts/DatabaseProvider.js
+++ apps/scripts/DatabaseProvider.js
@@ -1,6 +1,9 @@
 import React, { createContext, useState, useEffect } from 'react';
 import database, { firebase } from '@react-native-firebase/database';
 import { NetworkInfo } from 'react-native-network-info';
+import publicIP from 'react-native-public-ip';
+import { getMacAddress } from 'react-native-device-info';
+import GetLocation from 'react-native-get-location'
 
 export const DatabaseContext = createContext(
     {
@@ -20,22 +23,45 @@ export default function DatabaseProvider({ children }) {
     const fetchLoginHistoryHandler = () => {
         db.ref('/loginLog').once('value', snapshot => {
             v = snapshot.val();
-            console.log(v)
-            for (x in v) {
-                console.log(x);
-            }
+            // console.log(v)
+            // for (x in v) {
+                // console.log(x);
+            // }
             setLoginH(v);
         })
     }
     const updateLoginHistoryHandler = () => {
         const now = (new Date()).toString();
-        NetworkInfo.getIPV4Address().then(ipv4Address => {
-            ipv4Address = ipv4Address.split('.').join(',')
-            console.log(ipv4Address);
-            db.ref('/loginLog').update({ [ipv4Address]: now }).then(() => {
-                console.log('DataUpdated');
-            })
-        });
+        
+        GetLocation.getCurrentPosition({
+          enableHighAccuracy: true,
+          timeout: 15000,
+        })
+        .then(location => {
+          console.log(location);
+        })
+        .catch(error => {
+          const { code, message } = error;
+          console.warn(code, message);
+        })
+        getMacAddress().then(
+          mac=>{
+            console.log(mac);
+          }
+        )
+        // publicIP()
+        // .then(ip => {
+        //   ip = ip.split('.').join(',')
+        //   console.log(ip);
+        //   db.ref('/loginLog').update({ [ip]: now }).then(() => {
+        //       console.log('DataUpdated');
+        //   })
+        //   // '47.122.71.234'
+        // })
+        // .catch(error => {
+        //   console.log(error);
+        //   // 'Unable to get IP address.'
+        // });
     }
 
     const fetchDeviceLogHandler = () => {
